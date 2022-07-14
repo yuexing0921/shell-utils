238957a76b56c363060b175aba7d39300e26f74a
kerwin.yi <kerwin.yi@bybit.com>
2022-07-14 18:01:40 +0800

    docs: update

--- delete-branch.sh
+++ delete-branch.sh
@@ -11,7 +11,6 @@ for branch in $(git branch -a | grep -v HEAD | grep -v master | sed /\*/d); do
         if [ -z "$(git log -1 --since='April 1, 2022' -s ${branch})" ]; then
                 echo -e `git show --format="%ci %cr %an" ${branch} | head -n 1` \\t$branch
                 remote_branch=$(echo ${branch} | sed 's#remotes/origin/##' )
-                # echo 123 ${remote_branch}
                 # To delete the branches uncomment the bellow git delete command
                 git push origin --delete ${remote_branch}
         fi
--- delete-tag.sh
+++ delete-tag.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # chmod 777 delete-tag.sh
 # Script to delete remote git tag
-##
-# 先删除本地的所有分支
+
+## remote all local tag
 # git tag | xargs git tag -d
+
 # Fetch the remote resources
 git fetch
 
@@ -12,7 +13,6 @@ for branch in $(git tag -l | sed /\*/d); do
         if [ -z "$(git log -1 --since='May 1, 2022' -s ${branch})" ]; then
                 echo -e `git show --format="%ci %cr %an" ${branch} | head -n 1` \\t$branch
                 remote_branch=$(echo ${branch} | sed 's#remotes/origin/##' )
-                # echo 123 ${remote_branch}
                 # To delete the branches uncomment the bellow git delete command
                 git push origin --delete ${remote_branch}
         fi
