94a4686d2fa50dec00a8025c746398965ccdef83
kerwin.yi <kerwin.yi@bybit.com>
2022-07-14 17:02:03 +0800

    feat

---
+++ delete-branch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# chmod 777 delete-branch.sh
+# Script to delete remote git branches
+##
+
+# Fetch the remote resources
+git fetch
+
+# Loop through all remote branches
+for branch in $(git branch -a | grep -v HEAD | grep -v master | sed /\*/d); do
+        if [ -z "$(git log -1 --since='April 1, 2022' -s ${branch})" ]; then
+                echo -e `git show --format="%ci %cr %an" ${branch} | head -n 1` \\t$branch
+                remote_branch=$(echo ${branch} | sed 's#remotes/origin/##' )
+                # echo 123 ${remote_branch}
+                # To delete the branches uncomment the bellow git delete command
+                git push origin --delete ${remote_branch}
+        fi
+done
---
+++ readme.md
@@ -0,0 +1,5 @@
+## 收集一些批量操作的git 命令，方便管理
+
+### 批量删除指定日期之后的tag delete-tag.sh
+
+### 批量删除指定日期之后的branch delete-branch.sh
